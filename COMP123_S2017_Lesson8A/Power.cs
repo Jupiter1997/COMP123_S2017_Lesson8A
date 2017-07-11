98f2c51b7d4a2de195c2685a934f3d7c40d2febb
Jupiter1997 <jupiter.jv@gmail.com>
2017-07-11 15:46:47 -0400

    Added Power Data Type

---
+++ COMP123_S2017_Lesson8A/Power.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name: Marvin Jupiter Vargas
+ * Date: July 11, 2017
+ * Description:This is a new Power Data type
+ * Version: 0.1 - Created the Power
+ * */
+namespace COMP123_S2017_Lesson8A
+{
+    public class Power
+    {
+        /// <summary>
+        /// The power SuperClass contains the Data and structure of each super Power
+        /// </summary>
+        public string Name { get; set; }
+        public int Rank { get; set; }
+    }
+}
