ca206e18c4411f190c38aeaca8a5c57839fbbe88
Jupiter1997 <jupiter.jv@gmail.com>
2017-07-11 15:27:41 -0400

    Added Human Abstract Class

---
+++ COMP123_S2017_Lesson8A/Humans.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name: Marvin Jupiter Vargas
+ * Date: July 11, 2017
+ * Description: Abstarct Class Human which other sub class will derived from
+ * 
+ * 
+ * /
+namespace COMP123_S2017_Lesson8A
+{
+    /// <summary>
+    /// This is the Human Abstarct Class
+    /// </summary>
+    public abstract class Humans
+    {
+        //PRIVATE FIELDS(INSTANCE VARIABLES)
+        private string _name;
+
+        //PUBLIC PROPERTIES
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+        //CONSTRUSTERS--->
+        public Humans(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
