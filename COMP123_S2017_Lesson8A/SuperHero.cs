86eeb9914ad1b07a95414307cd9d9404a77e8868
Jupiter1997 <jupiter.jv@gmail.com>
2017-07-13 11:47:47 -0400

    Superhero subclass

---
+++ COMP123_S2017_Lesson8A/SuperHero.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name: Marvin Jupiter Vargas
+ * Date: July 13, 2017
+ * Description: Super heror class which inherits from superhuman class
+ * Version: 0.7 - override void display skills
+ * */
+namespace COMP123_S2017_Lesson8A
+{
+    /// <summary>
+    /// this is the superhero class
+    /// </summary>
+    public class SuperHero : SuperHuman, IHasKarma
+    {
+        //PRIVATE INSTANCE VARIABLE 
+        private int _karma;
+        //PUBLIC PROPERTIES
+        public int Karma
+        {
+            get
+            {
+                return this._karma;
+            }
+            set
+            {
+                this._karma = value;
+            }
+        }
+
+        //CONSTRUCTERS
+        /// <summary>
+        /// This is the main constructer for the super hero class
+        /// it takes one argument name(string) and karma(int)
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="karma"></param>
+        public SuperHero(string name, int karma)
+            :base(name)
+        {
+            this.Karma = karma;
+        }
+        //PRIVATE METHODS
+        
+        //PUBLIC METHODS
+         
+    }
+}
