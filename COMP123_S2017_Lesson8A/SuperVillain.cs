191a567fdf7ddf8d0de57f4cfc99dff4ab33ad13
Jupiter1997 <jupiter.jv@gmail.com>
2017-07-13 12:02:12 -0400

    Made a SuperVillan Class and its constructer

---
+++ COMP123_S2017_Lesson8A/SuperVillain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Marvin Jupiter Vargas
+ * Date: July 13, 2017
+ * Description: this is the SuperVillain Class
+ * Version: 0.11 - added a constructer
+ */
+namespace COMP123_S2017_Lesson8A
+{
+    public class SuperVillain : SuperHuman
+    {
+
+        //PRIVATE INSTANCE VARIABLE 
+       
+        //PUBLIC PROPERTIES
+      
+        //CONSTRUCTERS
+        public SuperVillain(string name)
+            :base(name)
+        {
+
+        }
+        //PRIVATE METHODS
+
+        //PUBLIC METHODS
+
+    }
+
+}
