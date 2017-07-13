9c17ba5454ce05bdb86d58f3e44a8a3883407821
Jupiter1997 <jupiter.jv@gmail.com>
2017-07-13 12:08:00 -0400

    Added Malice Contructer

--- COMP123_S2017_Lesson8A/IHasMalice.cs
+++ COMP123_S2017_Lesson8A/IHasMalice.cs
@@ -6,11 +6,14 @@ using System.Text;
  * Name: Marvin Jupiter Vargas
  * Date: July 13, 2017
  * Description: Interface IHasMalice created
- * Version: 0.12 
+ * Version: 0.12  added a property malice
  */
 namespace COMP123_S2017_Lesson8A
 {
     public interface IHasMalice
     {
+        //PUBLIC PROPERTY
+        int Malice { get; set; }
+
     }
 }
