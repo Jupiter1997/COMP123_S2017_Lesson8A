49f4a87a2f7091c6e312b33225222630bc2f91ed
Jupiter1997 <jupiter.jv@gmail.com>
2017-07-13 12:06:44 -0400

    Added interface IHasMalice

---
+++ COMP123_S2017_Lesson8A/IHasMalice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name: Marvin Jupiter Vargas
+ * Date: July 13, 2017
+ * Description: Interface IHasMalice created
+ * Version: 0.12 
+ */
+namespace COMP123_S2017_Lesson8A
+{
+    public interface IHasMalice
+    {
+    }
+}
